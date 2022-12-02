9750952e95b577c9975ab84c7c8c4b4669c9f5d4
Jack Jones <jackmj@proton.me>
2022-12-01 18:17:40 -0600

    forgot to push the changes like months ago lmao

--- source/Main.cs
+++ source/Main.cs
@@ -1,18 +1,49 @@
 using Godot;
 
 public partial class Main : Node3D {
+    [Export] private bool gameStarted = false;
+    internal bool GameStarted => gameStarted;
+	private static byte lapCount = 3; 
+    internal static byte LapCount => lapCount;
+    [Export] internal int numFinished = 0; // Number of players that have finished the race. Used for place counting.
+    
     [Export] private PackedScene playerObject;
     private const ushort PORT = 42069;
     
     private ENetMultiplayerPeer peer = new();
+    internal ENetMultiplayerPeer Peer => peer;
+    
+    private Label statusLabel;
+    private VBoxContainer netMenu;
     private LineEdit ipField;
 
     public override void _Ready() {
+        netMenu = GetNode<VBoxContainer>("NetworkMenu");
         ipField = GetNode<LineEdit>("NetworkMenu/IPField");
+        statusLabel = GetNode<Label>("StatusLabel");
         
         // Bind the window resize signal.
         GetTree().Root.Connect("size_changed", new Callable(this,nameof(ResizeViewport))); 
         ResizeViewport(); // Set the initial viewport size.
+    }
+
+    public override void _Process(double _delta) {
+        if(!gameStarted && Input.IsActionJustPressed("start") && IsMultiplayerAuthority()) {
+            StartGame();
+        }
+
+        base._Process(_delta);
+    }
+    
+    private async void StartGame(int _countdown = 3) {
+        for(int _i = 0; _i < _countdown; _i++) {
+            statusLabel.Text = $"Race starting in {_countdown - _i}...";
+            await ToSignal(GetTree().CreateTimer(1), "timeout");
+        }
+        
+        statusLabel.Hide();
+        gameStarted = true;
+
     }
 
     // Called automatically when the user resizes the window.
@@ -25,6 +56,9 @@ public partial class Main : Node3D {
     
     // Called automatically when the user presses the "Host" button.
     private void Host() {
+        netMenu.Hide();
+        statusLabel.Show();
+        statusLabel.Text = "The host can press START button or ENTER key to start the race!";
         peer.CreateServer(PORT);
         Multiplayer.MultiplayerPeer = peer;
 
@@ -34,6 +68,7 @@ public partial class Main : Node3D {
     
     // Called automatically when the user presses the "Join" button.
     private void Join() {
+        netMenu.Hide();
         peer.CreateClient(ipField.Text, PORT);
         Multiplayer.MultiplayerPeer = peer;
     }
--- source/ship/Ship.cs
+++ source/ship/Ship.cs
@@ -11,12 +11,18 @@ public partial class Ship : RigidBody3D {
 
 	private float baseDamping; // Stores the linear damping value on the RigidBody and uses it for the braking mechanic.
 
+	[Export] private sbyte currentLap = 1;
+	private bool finishedRace = false;
+
+	private Main main;
+	
 	// Child nodes
 	private Camera3D camera;
 	private Node3D gfx;
 	private SubViewport subview;
 	private MultiplayerSynchronizer synchronizer;
-	
+    private Label lapLabel;
+
 	#region Godot Overrides
 
 	public override void _EnterTree() {
@@ -25,18 +31,31 @@ public partial class Ship : RigidBody3D {
 		gfx = GetNode<Node3D>("GFX");
 		subview = GetTree().Root.GetNode<SubViewport>("Main/SubViewportContainer/SubViewport");
 		synchronizer = GetNode<MultiplayerSynchronizer>("MultiplayerSynchronizer");
-		
+		lapLabel = GetNode<Label>("LapLabel");	
 		synchronizer.SetMultiplayerAuthority(Name.ToString().ToInt());
+		main = GetTree().Root.GetNode<Main>("Main");
 		
 		baseDamping = LinearDamp; // Sets baseDamping based on the RigidBody's linear damping setting in the inspector.
+		GetTree().Root.GetNode<Area3D>("Main/FinishLine/FinishArea").BodyEntered += CrossFinish;
+		ConstantForce += Vector3.Left * 0.1f; // Prevents from spawning within other ships
 		base._Ready();
 	}
 
 	public override void _PhysicsProcess(double _delta) {
 		camera.Current = synchronizer.IsMultiplayerAuthority();
-		if(synchronizer.IsMultiplayerAuthority() && controlMode != ControlMode.Disabled) {
+		if(synchronizer.IsMultiplayerAuthority() && controlMode != ControlMode.Disabled && main.GameStarted) {
 			Steer();
 			Accelerate();
+		}
+		
+		if(main.GameStarted && !lapLabel.Visible && synchronizer.IsMultiplayerAuthority()) {
+			lapLabel.Show();
+			lapLabel.Text = $"Lap 1/{Main.LapCount}";
+		}
+
+		if(finishedRace && Input.IsActionJustPressed("start")) {
+			main.Peer.CloseConnection();
+			GetTree().ReloadCurrentScene();
 		}
 
 		Position *= new Vector3(1, 0, 1); // Kinda weird way of locking the Y position	
@@ -56,7 +75,7 @@ public partial class Ship : RigidBody3D {
 	}
 	
 	#endregion
-	
+
 	#region Movement
 	
 	// Handle moving and braking.
@@ -80,7 +99,7 @@ public partial class Ship : RigidBody3D {
 			_direction.y = Input.GetJoyAxis(controllerIndex, JoyAxis.LeftY);
 
 			// A manual dead zone implementation is required to prevent direction changing when letting go of the joystick.
-			if(_direction.Abs().x > 0.5f || _direction.Abs().y > 0.5f) {
+			if(_direction.Abs().x > 0.75f || _direction.Abs().y > 0.75f) {
 				Vector3 _lookTarget = new(gfx.GlobalPosition.x + _direction.x, gfx.GlobalPosition.y,
 					gfx.GlobalPosition.z + _direction.y);
 				gfx.LookAt(_lookTarget); // Rotate in direction of input axis
@@ -104,6 +123,34 @@ public partial class Ship : RigidBody3D {
 	
 	#endregion
 
+	private void CrossFinish(Node _body) {
+		
+		float _dist = Position.DistanceTo(GetTree().Root.GetNode<Node3D>("Main/FinishLine").Position);
+		if(_dist > 12 || finishedRace)
+			return;
+		
+		if(ConstantForce.z <= 0) {
+			if(currentLap >= Main.LapCount) {
+				main.numFinished++;
+				lapLabel.Text = $"Finished in place #{main.numFinished}! Press START button or ENTER key to restart.";
+				finishedRace = true;
+			} else {
+				currentLap++; ;
+				lapLabel.Text = $"Lap {currentLap}/{Main.LapCount}";
+			}
+		} else {
+			currentLap -= 1;
+			if(currentLap <= 0) {
+				currentLap = 0;
+				lapLabel.Text = $"Lap 1/{Main.LapCount}";
+			} else {
+				lapLabel.Text = $"Lap {currentLap}/{Main.LapCount}";
+			}
+		}
+		
+		GD.Print($"Current Lap {currentLap}");
+	}
+	
 	private enum ControlMode {
 		Disabled,
 		Mouse,
